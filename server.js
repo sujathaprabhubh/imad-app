0ad2ea0f1784168b01183908728ace2017b9547a
sujathaprabhubh <sujathaprabhubh@gmail.com>
2017-09-04 23:40:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res){
     // articleName == article-one
     // articles[articleName] == {} content object for article-one
-   var articleName =  req. param . articleName;
+   var articleName =  req.params.articleName;
      res.send(createTemplate(articles[articleName]));
 });
 
