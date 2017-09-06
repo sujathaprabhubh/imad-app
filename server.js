7b2d2b33b1a310a16f6e3fc54cddd1f2fa27b905
sujathaprabhubh <sujathaprabhubh@gmail.com>
2017-09-06 13:44:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,6 +93,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/coutner', function(req , res){
+   counter= counter+1;
+   res.send(counter.toString());
+    
+});
+
 app.get('/:articleName', function (req, res){
     // articleName == article-one
     // articles[articleName] == {} content object for article-one
