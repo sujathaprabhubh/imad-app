7015c316bc047f1a6570a4172df42ddc4474d311
sujathaprabhubh <sujathaprabhubh@gmail.com>
2017-09-06 14:20:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/', function (req, res) {
 });
 
 var counter = 0;
-app.get('/coutner', function(req , res){
+app.get('/counter', function(req , res){
    counter= counter+1;
    res.send(counter.toString());
     
